5e03696820e98062c6296479be0da9482767d5e0
rburcea <robert.burcea@gmail.com>
2022-11-15 15:29:46 +0200

    make screen touch-none

--- src/pages/Costuri.jsx
+++ src/pages/Costuri.jsx
@@ -5,7 +5,7 @@ const Costuri = () => {
   const data = useData();
   const setData = useSetData();
   return (
-    <div className="w-full h-full max-w-[80%] bg-transparent mx-[10%] my-[5%] flex flex-col items-center rounded-xl shadow-xl">
+    <div className="w-full max-w-[80%] bg-transparent mx-[10%] my-[5%] flex flex-col items-center rounded-xl shadow-xl">
       <div className="w-full max-w-[98%] my-2 p-2 border rounded shadow-xl flex flex-col items-center bg-gray-200 bg-opacity-60">
         <h1>GAZE</h1>
         <table className="bg-gray-300 rounded-xl p-2">
@@ -36,14 +36,18 @@ const Costuri = () => {
       <div className="w-full max-w-[98%] my-2 p-2 border rounded shadow-xl flex flex-col items-center bg-gray-200 bg-opacity-60">
         <h1>CURENT</h1>
         <table className="bg-gray-300 rounded-xl p-2">
-          <tr>
-            <th className="py-3 px-1 border-b">Index Vechi</th>
-            <th className="py-3 px-1 border-b">Total Consum</th>
-          </tr>
-          <tr>
-            <td className="border-b text-xl text-green-800">432</td>
-            <td className="border-b text-xl text-green-800">345 RON</td>
-          </tr>
+          <thead>
+            <tr>
+              <th className="py-3 px-1 border-b">Index Vechi</th>
+              <th className="py-3 px-1 border-b">Total Consum</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border-b text-xl text-green-800">432</td>
+              <td className="border-b text-xl text-green-800">345 RON</td>
+            </tr>
+          </tbody>
         </table>
         <div className="flex flex-col items-center">
           <p className="m-2">Introdu index nou</p>
